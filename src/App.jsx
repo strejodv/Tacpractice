b2bf0c7594f36b684f860afae71bdb1585757b15
Stefano Trejo <stides11@gmail.com>
2024-06-25 23:52:56 -0700

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "./Hero.jsx"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <HeroSection ></HeroSection>
+    </>
+  )
+}
+
+export default App
---
+++ src/Hero.jsx
@@ -0,0 +1,29 @@
+import Pet from './assets/Pet.png';
+
+
+function HeroSection () {
+  
+
+    return (
+    <>  
+    <nav className="hero-nav">
+        <h3>TacPractice</h3>
+    </nav>
+    <div className="hero-section">
+        <div className="left-section">
+            <h1>Enhance Your Communication Skills With AI</h1>
+            <div>
+                <button>Start</button><button>More Info</button>
+            </div>    
+            <p>Non-violent Communication Training for Future Police Officers</p>
+        </div>
+            <div className="hero-pet">
+        <img  className="hero-pet" src={Pet}></img>
+            </div>
+    </div>
+    <footer><p className="hero-footer">StefanoTrejo made by</p></footer>
+    </>
+    )
+  }
+  
+  export default HeroSection
