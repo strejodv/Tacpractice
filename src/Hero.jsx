473926913ed7969527ef0e11204d576852f5420e
Stefano Trejo <stides11@gmail.com>
2024-06-27 20:03:16 -0700

    Added input element for username on landing page

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,19 +7,20 @@ function HeroSection () {
     return (
     <>  
     <nav className="hero-nav">
-        <h3>TacPractice</h3>
+        <h3>TACPractice</h3>
     </nav>
     <div className="hero-section">
         <div className="left-section">
             <h1>Enhance Your Communication Skills With AI</h1>
-            <div>
-                <button>Start</button><button>More Info</button>
-            </div>    
-            <p>Non-violent Communication Training for Future Police Officers</p>
+                <div>
+                    <button>Start</button><button>More Info</button>
+                </div>
+                    <input className="username-input" input type="text" placeholder="Please type your name and press 'Start'" id="username" name="username" required></input>
+                        <p>Non-violent Communication Training for Future Police Officers</p>
         </div>
-            <div className="hero-pet">
-        <img  className="hero-pet" src={Pet}></img>
-            </div>
+                <div className="hero-pet">
+                    <img  className="hero-pet" src={Pet}></img>
+                </div>
     </div>
     <footer><p className="hero-footer">StefanoTrejo made by</p></footer>
     </>
